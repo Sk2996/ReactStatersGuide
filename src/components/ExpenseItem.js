d9bfe57ebff0b0ab32b00fec25be107f8759e58a
sk2996 <sksaikrishhna53@gmail.com>
2021-05-11 21:09:21 +0530

    committing after first optimisation

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css'
 
+import ExpenseDate from './ExpenseDate';
+
 function ExpenseItem(props) { // this name props can be anything like date,data,expences etc but i prefer props
     return (
         <div className="expense-item">
-            <div>March 28th 2021</div>
+            <ExpenseDate date = {props.date}/>
+            
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.67</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
